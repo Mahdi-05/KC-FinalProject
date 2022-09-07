3e355f116312367ed60f3cc1e5a49e4cbdf64f76
Mahdi-05 <109863442+Mahdi-05@users.noreply.github.com>
2022-09-07 21:59:55 +0300

    Done

---
+++ JS/CpuPage.js
@@ -0,0 +1,96 @@
+CPUs = [
+  {
+    name: ` Ryzen 5 5600`,
+    pic: `/imgs/R5 5600.png `,
+    aPrice: ` $157.94 `,
+    nPrice: ` $159.99 `,
+    qPrice: ` KD 69.750 `,
+    alink: ` https://www.amazon.com/AMD-5600-12-Thread-Unlocked-Processor/dp/B09VCHR1VH/ref=sr_1_1_sspa?crid=1O45VA58FN3NP&keywords=AMD+Ryzen+5+5600&qid=1662453766&sprefix=amd+ryzen+5+5600%2Caps%2C264&sr=8-1-spons&psc=1 `,
+    nlink: ` https://www.newegg.com/amd-ryzen-5-5600-ryzen-5-5000-series/p/N82E16819113736 `,
+    qlink: ` https://quadrastores.com/index.php/home/product_view/7723/AMD-Ryzen-5-5600-36GHz-6-Cores-AM4-CPU-Desktop-Processor `,
+  },
+  {
+    name: ` Ryzen 7 5800X `,
+    pic: `/imgs/R7 5800X.png`,
+    aPrice: `$245.99 `,
+    nPrice: `$289.00 `,
+    qPrice: `Out Of Stock `,
+    alink: `https://www.amazon.com/AMD-Ryzen-5800X-16-Thread-Processor/dp/B0815XFSGK/ref=sr_1_1?crid=I5Y2AUI8DRU7&keywords=AMD+Ryzen+7+5700X+8-Core+3.4GHz+AM4+CPU+Desktop+Processor&qid=1662568910&sprefix=amd+ryzen+7+5700x+8-core+3.4ghz+am4+cpu+desktop+processor%2Caps%2C245&sr=8-1 `,
+    nlink: `https://www.newegg.com/amd-ryzen-7-5800x/p/N82E16819113665 `,
+    qlink: `https://quadrastores.com/index.php/home/product_view/5609/AMD-Ryzen-7-5800X-38-GHz-8-core-AM4-Processor `,
+  },
+  {
+    name: `R5 5500 `,
+    pic: `/imgs/R5 5500.png `,
+    aPrice: ` $138.00`,
+    nPrice: ` $138.96`,
+    qPrice: `KD 56.000 `,
+    alink: ` https://www.amazon.com/AMD-5500-12-Thread-Unlocked-Processor/dp/B09VCJ171S/ref=sr_1_1?keywords=AMD+Ryzen+5+5500+6-Cores+3.6GHz+AM4+CPU+Desktop+Processor&qid=1662569281&sr=8-1`,
+    nlink: ` https://www.newegg.com/amd-ryzen-5-5500-ryzen-5-5000-series/p/N82E16819113737?Description=AMD%20Ryzen%205%205500%206-Cores%203.6GHz%20AM4%20CPU%20Desktop%20Processor&cm_re=AMD_Ryzen%205%205500%206-Cores%203.6GHz%20AM4%20CPU%20Desktop%20Processor-_-19-113-737-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7725/AMD-Ryzen-5-5500-6-Cores-36GHz-AM4-CPU-Desktop-Processor `,
+  },
+  {
+    name: `R9 5900X `,
+    pic: `/imgs/R9 5900X.png `,
+    aPrice: ` $387.99`,
+    nPrice: `$399.99 `,
+    qPrice: `KD 184.000 `,
+    alink: ` https://www.amazon.com/AMD-Ryzen-5900X-24-Thread-Processor/dp/B08164VTWH/ref=sr_1_1?keywords=AMD%2BRyzen%2B9%2B5900X%2B3.7%2BGHz%2B12%2BCore%2BAM4%2BProcessor&qid=1662571284&sr=8-1&th=1`,
+    nlink: `https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664?Description=AMD%20Ryzen%209%205900X&cm_re=AMD_Ryzen%209%205900X-_-19-113-664-_-Product `,
+    qlink: `https://quadrastores.com/index.php/home/product_view/5610/AMD-Ryzen-9-5900X-37-GHz-12-Core-AM4-Processor `,
+  },
+  {
+    name: `intel core i5 12400 `,
+    pic: `/imgs/i5 12400.png `,
+    aPrice: ` $193.95`,
+    nPrice: `$179.00 `,
+    qPrice: `KD 69.000 `,
+    alink: `https://www.amazon.com/Intel-i5-12400-Desktop-Processor-Cache/dp/B09NMPD8V2/ref=sr_1_2?keywords=intel%2Bcore%2Bi5-12400%2B6-cores%2Blga1700%2B12th%2Bgen%2Bprocessor&qid=1662570199&sr=8-2&th=1 `,
+    nlink: `https://www.newegg.com/intel-core-i5-12400f-core-i5-12th-gen/p/N82E16819118358?Description=ntel%20Core%20i5-12400%206-Cores%20LGA1700%2012th%20Gen%20Processor&cm_re=ntel_Core%20i5-12400%206-Cores%20LGA1700%2012th%20Gen%20Processor-_-19-118-358-_-Product&quicklink=true `,
+    qlink: ` https://quadrastores.com/index.php/home/product_view/7329/Intel-Core-i5-12400-6-Cores-LGA1700-12th-Gen-Processor`,
+  },
+  {
+    name: `intel core i7 11700 `,
+    pic: ` /imgs/i7 11700.png`,
+    aPrice: `$299.00 `,
+    nPrice: `$349.99 `,
+    qPrice: `KD 105.000 `,
+    alink: ` https://www.amazon.com/Intel-i7-11700-Desktop-Processor-LGA1200/dp/B08X6QHYDL/ref=sr_1_2?crid=AH139R9A9CN9&keywords=intel+core+i7-11700+2.5+ghz+8-core+processor&qid=1662570556&sprefix=Intel+Core+i7-11700+%2Caps%2C425&sr=8-2`,
+    nlink: `https://www.newegg.com/intel-core-i7-11700-core-i7-11th-gen/p/N82E16819118237?Description=Intel%20Core%20i7-11700%202.5%20GHz%208-Core%20LGA%201200%2011th%20Gen%20Processor&cm_re=Intel_Core%20i7-11700%202.5%20GHz%208-Core%20LGA%201200%2011th%20Gen%20Processor-_-19-118-237-_-Product&quicklink=true `,
+    qlink: `https://quadrastores.com/index.php/home/product_view/6141/Intel-Core-i7-11700-25-GHz-8-Core-LGA-1200-11th-Gen-Processor`,
+  },
+  {
+    name: `intel core i9 11900K `,
+    pic: ` /imgs/i9 11900K.png`,
+    aPrice: `373.99 `,
+    nPrice: ` $349.99`,
+    qPrice: `KD 182.000 `,
+    alink: ` https://www.amazon.com/Intel-i9-11900K-Processor-Threads-5-30GHz/dp/B08TX48X3N/ref=sr_1_2?crid=2L1H5GKRQUK9K&keywords=Intel+Core+i9-11900K+3.5+GHz+8-Core+LGA+1200+11th+Gen+Processor&qid=1662570858&sprefix=intel+core+i9-11900k+3.5+ghz+8-core+lga+1200+11th+gen+processor%2Caps%2C516&sr=8-2`,
+    nlink: `https://www.newegg.com/intel-core-i9-11900k-core-i9-11th-gen/p/N82E16819118231?Description=Intel%20Core%20i9-11900K%203.5%20GHz%208-Core%20LGA%201200%2011th%20Gen%20Processor&cm_re=Intel_Core%20i9-11900K%203.5%20GHz%208-Core%20LGA%201200%2011th%20Gen%20Processor-_-19-118-231-_-Product `,
+    qlink: ` https://quadrastores.com/index.php/home/product_view/6132/Intel-Core-i9-11900K-35-GHz-8-Core-LGA-1200-11th-Gen-Processor`,
+  },
+  {
+    name: ` intel core i5 12600K`,
+    pic: ` /imgs/i5 12600K.png`,
+    aPrice: `$269.99 `,
+    nPrice: `$248.00`,
+    qPrice: `Out Of Stock`,
+    alink: ` https://www.amazon.com/Intel-i5-12600K-Desktop-Processor-Unlocked/dp/B09FX4D72T/ref=sr_1_3?crid=2NR4D5GUJTNOX&keywords=Intel%2BCore%2Bi5-12600K&qid=1662571729&sprefix=intel%2Bcore%2Bi5-12600k%2B%2Caps%2C263&sr=8-3&th=1`,
+    nlink: `https://www.newegg.com/intel-core-i5-12600k-core-i5-12th-gen/p/N82E16819118347?Description=Intel%20Core%20i5-12600K&cm_re=Intel_Core%20i5-12600K-_-19-118-347-_-Product&quicklink=true `,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7043/Intel-Core-i5-12600K-Box-10-Core-LGA-1700-12th-Gen-CPU-Processor `,
+  },
+];
+let box = document.getElementById(`box`);
+function displayCards() {
+  CPUs.forEach((element) => {
+    box.innerHTML += ` <div class="card">
+          <img src="${element.pic}">
+          <h5> ${element.name} </h5>
+          <a href=${element.alink} target="_blank"><h6>amazon:${element.aPrice}  </h6></a>
+          <a href=${element.nlink} target="_blank"> <h6>newegg:${element.nPrice} </h6></a>
+          <a href=${element.qlink} target="_blank"> <h6>quadra:${element.qPrice}</h6></a>
+  
+        </div>`;
+  });
+}
+displayCards();
---
+++ JS/GpuPage.js
@@ -0,0 +1,96 @@
+cards = [
+  {
+    cardName: ` MSI RTX 3060 12G`,
+    pic: `/imgs/MSI_3060_12G.png`,
+    aPrice: `$406.58`,
+    nPrice: `$379.99`,
+    qPrice: `KD 163`,
+    alink: `https://www.amazon.com/MSI-GeForce-RTX-3060-12G/dp/B08WPRMVWB/ref=sr_1_1?crid=1M69MODBCBNM0&keywords=MSI+Ventus+GeForce+RTX+3060+12GB+GDDR6+PCI+Express+4.0+Video+Card+RTX+3060+Ventus+2X+12G+OC&qid=1662304834&sprefix=msi+ventus+geforce+rtx+3060+12gb+gddr6+pci+express+4.0+video+card+rtx+3060+ventus+2x+12g+oc%2Caps%2C530&sr=8-1`,
+    nlink: `https://www.newegg.com/msi-geforce-rtx-3060-rtx-3060-ventus-2x-12g-oc/p/N82E16814137632?Description=3060&cm_re=3060-_-14-137-632-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/6037/MSI-GeForce-RTX-3060-GAMING-X-12GB-Graphics-Card`,
+  },
+  {
+    cardName: ` ZOTAC RTX 3060 12G `,
+    pic: `/imgs/ZOTAC_3060_12G.png`,
+    aPrice: `$423.97`,
+    nPrice: `$419.67`,
+    qPrice: `      NA`,
+    alink: `https://www.amazon.com/ZOTAC-GeForce-Graphics-IceStorm-ZT-A30600H-10M/dp/B08W8DGK3X/ref=sr_1_2?keywords=RTX%2B3000&qid=1662483377&s=pc&sr=1-2&th=1`,
+    nlink: `https://www.newegg.com/p/1FT-000M-00375?Description=zotac%203060%2012G&cm_re=zotac_3060%2012G-_-9SIAFVFGW32167-_-Product&quicklink=true`,
+    qlink: ``,
+  },
+  {
+    cardName: ` MSI RTX 3080 10G `,
+    pic: `/imgs/MSI_RTX_3080.png`,
+    aPrice: `$769.99`,
+    nPrice: `$739.98`,
+    qPrice: `Out Of Stock`,
+    alink: `https://www.amazon.com/MSI-GeForce-RTX-3080-LHR/dp/B0995S7548/ref=sr_1_3?keywords=RTX%2B3000&qid=1662492897&sr=8-3`,
+    nlink: `https://www.newegg.com/msi-geforce-rtx-3080-rtx-3080-gaming-z-trio-10g-lhr/p/N82E16814137677?Description=MSI%20RTX%203080&cm_re=MSI_RTX%203080-_-14-137-677-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7581/MSI-GeForce-RTX-3080-GAMING-Z-TRIO-10G-LHR-Graphics-Card`,
+  },
+  {
+    cardName: ` ASUS RTX 3080  `,
+    pic: `/imgs/ASUS_RTX_3080.png`,
+    aPrice: `$799.0`,
+    nPrice: `OUT OF STOCK`,
+    qPrice: `KD 359`,
+    alink: `https://www.amazon.com/ASUS-Graphics-DisplayPort-Axial-tech-2-9-Slot/dp/B09QH9NT3V/ref=sr_1_1?crid=1EEFF30W03PP&keywords=ASUS+ROG+Strix+RTX+3080+12GB+GDDR6X+Graphic+Card&qid=1662494060&sprefix=asus+rog+strix+rtx+3080+12gb+gddr6x+graphic+card%2Caps%2C732&sr=8-1`,
+    nlink: `https://www.newegg.com/asus-geforce-rtx-3080-rog-strix-rtx3080-o12g-gaming/p/N82E16814126550?Description=ASUS%20ROG%20Strix%20RTX%203080%2012GB%20GDDR6X%20Graphic%20Card&cm_re=ASUS_ROG%20Strix%20RTX%203080%2012GB%20GDDR6X%20Graphic%20Card-_-14-126-550-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7624/ASUS-ROG-Strix-RTX-3080-12GB-GDDR6X-Graphic-Card`,
+  },
+  {
+    cardName: `GIGABYTE RTX 3080`,
+    pic: `/imgs/GIGABYTE RTX 3080.png`,
+    aPrice: `$1,193.82`,
+    nPrice: `$889.99`,
+    qPrice: `KD 315`,
+    alink: `https://www.amazon.com/GIGABYTE-Graphics-WINDFORCE-GV-N3080GAMING-OC-12GD/dp/B09QDWGNPG/ref=sr_1_5?crid=2OX1MYNXK6YL&keywords=Gigabyte+GeForce+RTX+3080+Gaming+OC+12GB+GDDR6X+Graphics+Card&qid=1662544552&sprefix=gigabyte+geforce+rtx+3080+gaming+oc+12gb+gddr6x+graphics+card%2Caps%2C580&sr=8-5`,
+    nlink: `https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=Gigabyte%20GeForce%20RTX%203080%20Gaming%20OC%2012GB%20GDDR6X%20Graphics%20Card&cm_re=Gigabyte_GeForce%20RTX%203080%20Gaming%20OC%2012GB%20GDDR6X%20Graphics%20Card-_-14-932-436-_-Product`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7340/Gigabyte-GeForce-RTX-3080-Gaming-OC-12GB-GDDR6X-Graphics-Card`,
+  },
+  {
+    cardName: `ASUS RTX 3050`,
+    pic: `/imgs/ASUS RTX 3050.png`,
+    aPrice: `$369.99`,
+    nPrice: `$369.99`,
+    qPrice: `KD 155`,
+    alink: `https://www.amazon.com/ASUS-NVIDIA-GeForce-Gaming-Graphics/dp/B09R92F6ZF/ref=sr_1_1?crid=ZZOG9JKCKFPQ&keywords=Asus+ROG+Strix+GeForce+RTX+3050+OC+Edition+8GB+Graphics+Card&qid=1662545490&sprefix=90yv0hi1-m0na00%2Caps%2C677&sr=8-1`,
+    nlink: `https://www.newegg.com/asus-geforce-rtx-3050-rog-strix-rtx3050-o8g-gaming/p/N82E16814126556?Description=Asus%20ROG%20Strix%20GeForce%20RTX%203050%20OC%20Edition%208GB%20Graphics%20Card&cm_re=Asus_ROG%20Strix%20GeForce%20RTX%203050%20OC%20Edition%208GB%20Graphics%20Card-_-14-126-556-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/7464/Asus-ROG-Strix-GeForce-RTX-3050-OC-Edition-8GB-Graphics-Card`,
+  },
+  {
+    cardName: ` ASUS TUF RTX 3090`,
+    pic: `/imgs/TUF RTX 3090.png`,
+    aPrice: `$1,099.99`,
+    nPrice: `$1,099.99`,
+    qPrice: `KD 799`,
+    alink: `https://www.amazon.com/ASUS-GeForce-Graphics-DisplayPort-Bearings/dp/B08HJGNJ81/ref=sr_1_1?crid=1UBHA8ITJC5IF&keywords=ASUS+GeForce+RTX+3090+TUF+Gaming+24GB+GDDR6X+PCI-Express+Graphics+Card&qid=1662546661&sprefix=asus+geforce+rtx+3090+tuf+gaming+24gb+gddr6x+pci-express+graphics+card%2Caps%2C693&sr=8-1`,
+    nlink: `https://www.newegg.com/asus-geforce-rtx-3090-tuf-rtx3090-o24g-gaming/p/N82E16814126454?Description=ASUS%20GeForce%20RTX%203090%20TUF%20Gaming%2024GB%20GDDR6X%20PCI-Express%20Graphics%20Card&cm_re=ASUS_GeForce%20RTX%203090%20TUF%20Gaming%2024GB%20GDDR6X%20PCI-Express%20Graphics%20Card-_-14-126-454-_-Product&quicklink=true`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/5542/ASUS-GeForce-RTX-3090-TUF-Gaming-24GB-GDDR6X-PCI-Express-Graphics-Card`,
+  },
+  {
+    cardName: `GIGABYTE RTX 3070`,
+    pic: `/imgs/GIGABYTE RTX 3070.png`,
+    aPrice: `$549.99`,
+    nPrice: `$549.99`,
+    qPrice: `KD 215`,
+    alink: `https://www.amazon.com/GIGABYTE-REV2-0-WINDFORCE-GV-N3070GAMING-OC-8GD/dp/B098Q4M4WH/ref=sr_1_1?keywords=Gigabyte+GeForce+RTX+3070+GAMING+OC+8G+Graphics+Card&qid=1662547158&sr=8-1`,
+    nlink: `https://www.newegg.com/gigabyte-geforce-rtx-3070-gv-n3070gaming-oc-8gd/p/N82E16814932449?Description=Gigabyte%20GeForce%20RTX%203070%20GAMING%20OC%208G%20Graphics%20Card&cm_re=Gigabyte_GeForce%20RTX%203070%20GAMING%20OC%208G%20Graphics%20Card-_-14-932-449-_-Product`,
+    qlink: `https://quadrastores.com/index.php/home/product_view/5577/Gigabyte-GeForce-RTX-3070-GAMING-OC-8G-Graphics-Card-Rev-20`,
+  },
+];
+let box = document.getElementById(`box`);
+function displayCards() {
+  cards.forEach((element) => {
+    box.innerHTML += ` <div class="card">
+        <img src="${element.pic}">
+        <h5> ${element.cardName} </h5>
+        <a href=${element.alink} target="_blank"><h6>amazon:${element.aPrice}  </h6></a>
+        <a href=${element.nlink} target="_blank"> <h6>newegg:${element.nPrice} </h6></a>
+        <a href=${element.qlink} target="_blank"> <h6>quadra:${element.qPrice}</h6></a>
+
+      </div>`;
+  });
+}
+displayCards();
